4b0916f095611139ed64220426803669eb0c7d66
wphkweerasinghe <hkalhara777@gmail.com>
2022-05-21 22:44:44 +0530

    Create UpdateTicketServlet.java

---
+++ PhoenixAirline/src/java/com/phoenixairline/controllers/ticketmanagement/UpdateTicketServlet.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.phoenixairline.controllers.ticketmanagement;
+
+import com.phoenixairline.models.Ticket;
+import com.phoenixairline.models.TicketDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Heshan Kalhara
+ */
+public class UpdateTicketServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet UpdateTicketServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateTicketServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String bookingId = request.getParameter("reservationId");
+        String userId = "";//get current userId and enter here
+        String fhacId = "";//get fhacId according booking table
+        String passportNumber = request.getParameter("passportNumber");
+        String date = request.getParameter("date");
+        String classType = request.getParameter("class");
+        int seats = Integer.parseInt(request.getParameter("seats"));
+        
+        Ticket ticketBean = new Ticket(bookingId,userId,fhacId,passportNumber,date,classType,seats);
+        TicketDao updateTicket = new TicketDao();
+        String updateResult = updateTicket.updateTicket(ticketBean);
+        PrintWriter out = response.getWriter();
+        out.println(updateResult);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
